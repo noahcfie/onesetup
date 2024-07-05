e1f62cc7f513fa436e45f6583a32d54f8a5912d1
fschlegelone <fabian@schlegel.one>
2024-07-05 11:28:23 +0200

    refactor: zsh role

--- roles/zsh/files/user_config/env.zsh
+++ roles/zsh/files/user_config/env.zsh
@@ -10,7 +10,7 @@ export HOSTNAME="mac-one"
 export USR="$USER"
 
 # -- XDG VARIABLES -- #
-# export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CACHE_HOME="$HOME/.cache"
