956e63b3211a37f13e052cc98d8ad8a4235d1076
fschlegelone <fabian@schlegel.one>
2024-07-10 12:54:14 +0200

    update nvim config

--- roles/nvim/files/lua/plugins/alpha.lua
+++ roles/nvim/files/lua/plugins/alpha.lua
@@ -1,48 +1,52 @@
 return {
-  "goolord/alpha-nvim",
-  branch = "main",
-  lazy = true,
-  event = "VimEnter",
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+	"goolord/alpha-nvim",
+	branch = "main",
+	lazy = true,
+	event = "VimEnter",
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-    -- Set header
-    dashboard.section.header.val = {
-      "                                                  ",
-      "                                                  ",
-      "                                                  ",
-      "⠤⣤⣤⣤⣄⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣠⣤⠤⠤⠴⠶⠶⠶⠶",
-      "⢠⣤⣤⡄⣤⣤⣤⠄⣀⠉⣉⣙⠒⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠴⠘⣉⢡⣤⡤⠐⣶⡆⢶⠀⣶⣶⡦",
-      "⣄⢻⣿⣧⠻⠇⠋⠀⠋⠀⢘⣿⢳⣦⣌⠳⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠞⣡⣴⣧⠻⣄⢸⣿⣿⡟⢁⡻⣸⣿⡿⠁",
-      "⠈⠃⠙⢿⣧⣙⠶⣿⣿⡷⢘⣡⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣷⣝⡳⠶⠶⠾⣛⣵⡿⠋⠀⠀",
-      "⠀⠀⠀⠀⠉⠻⣿⣶⠂⠘⠛⠛⠛⢛⡛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠛⠀⠉⠒⠛⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⢸⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⢻⡁⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "⠀⠀⠀⠀⠀⠀⠀⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "                                                  ",
-      "                                                  ",
-    }
+		-- Set header
+		dashboard.section.header.val = {
+			"                                                  ",
+			"                                                  ",
+			"                                                  ",
+			"⠤⣤⣤⣤⣄⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣠⣤⠤⠤⠴⠶⠶⠶⠶",
+			"⢠⣤⣤⡄⣤⣤⣤⠄⣀⠉⣉⣙⠒⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠴⠘⣉⢡⣤⡤⠐⣶⡆⢶⠀⣶⣶⡦",
+			"⣄⢻⣿⣧⠻⠇⠋⠀⠋⠀⢘⣿⢳⣦⣌⠳⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠞⣡⣴⣧⠻⣄⢸⣿⣿⡟⢁⡻⣸⣿⡿⠁",
+			"⠈⠃⠙⢿⣧⣙⠶⣿⣿⡷⢘⣡⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣷⣝⡳⠶⠶⠾⣛⣵⡿⠋⠀⠀",
+			"⠀⠀⠀⠀⠉⠻⣿⣶⠂⠘⠛⠛⠛⢛⡛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠛⠀⠉⠒⠛⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⢸⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⢻⡁⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"⠀⠀⠀⠀⠀⠀⠀⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+			"                                                  ",
+			"                                                  ",
+		}
 
-    -- Set menu
-    dashboard.section.buttons.val = {
-      dashboard.button("e", " explorer", "<cmd>Neotree toggle<CR>"),
-      dashboard.button("f", " fuzzyfind", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("l", "󱐌 lazy", "<cmd>Lazy<CR>"),
-      dashboard.button("g", "󰊢 git", "<cmd>LazyGit<CR>"),
-      dashboard.button("c", " config", "<cmd>cd ~/.config | Neotree toggle<CR>"),
-      dashboard.button("n", " neovim", "<cmd>cd ~/.config/nvim | e ~/.config/nvim/init.lua | Neotree toggle<CR>"),
-      dashboard.button("q", " quit", "<cmd>qa!<CR>"),
-    }
+		-- Set menu
+		dashboard.section.buttons.val = {
+			dashboard.button("e", " explorer", "<cmd>Oil<CR>"),
+			dashboard.button("f", " fuzzyfind", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("l", "󱐌 lazy", "<cmd>Lazy<CR>"),
+			dashboard.button("g", "󰊢 git", "<cmd>LazyGit<CR>"),
+			dashboard.button("c", " config", "<cmd>cd ~/.config | Neotree toggle<CR>"),
+			dashboard.button(
+				"n",
+				" neovim",
+				"<cmd>cd ~/.config/nvim | e ~/.config/nvim/init.lua | Neotree toggle<CR>"
+			),
+			dashboard.button("q", " quit", "<cmd>qa!<CR>"),
+		}
 
-    -- Send config to alpha
-    alpha.setup(dashboard.opts)
+		-- Send config to alpha
+		alpha.setup(dashboard.opts)
 
-    -- Disable folding on alpha buffer
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-  end,
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+	end,
 }
--- roles/nvim/files/lua/plugins/oil.lua
+++ roles/nvim/files/lua/plugins/oil.lua
@@ -1,28 +1,30 @@
 return {
-  "stevearc/oil.nvim",
-  event = "VimEnter", -- lazy load after vim startup
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local oil = require("oil")
-    oil.setup({
-      default_file_explorer = true,
-      columns = {
-        "icon",
-      },
-      -- KEYMAPS
-    keymaps = {
-      ["?"] = "actions.show_help", -- show help menu
-      ["<CR>"] = "actions.select", -- open selected file/directory
-      ["<C-p>"] = "actions.preview", -- preview
-      ["<C-c>"] = "actions.close", -- close
-      ["<C-o>"] = "actions.open_external", -- open with default app
-      ["%"] = "actions.refresh", -- refresh
-      ["-"] = "actions.parent", -- move to parent directory
-      ["_"] = "actions.open_cwd", -- move to current working directory
-      ["cd"] = "actions.cd", -- change working directory
-      ["cs"] = "actions.change_sort", -- change sorting (name, size, ..)
-      ["H"] = "actions.toggle_hidden", -- show/hide hidden files
-    },
-    })
-  end
+	"stevearc/oil.nvim",
+	event = "VimEnter", -- lazy load after vim startup
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local oil = require("oil")
+		oil.setup({
+			default_file_explorer = true, -- replace netrw with oil
+			delete_to_trash = true, -- use the macos trash dir instead of completely removing the files
+			columns = {
+				"icon",
+			},
+			-- KEYMAPS
+			keymaps = {
+				["?"] = "actions.show_help", -- show help menu
+				["<CR>"] = "actions.select", -- open selected file/directory
+				["<C-p>"] = "actions.preview", -- preview
+				["<C-c>"] = "actions.close", -- close
+				["<C-o>"] = "actions.open_external", -- open with default app
+				["%"] = "actions.refresh", -- refresh
+				["<BS>"] = "actions.parent", -- move to parent directory
+				["_"] = "actions.open_cwd", -- move to current working directory
+				["cd"] = "actions.cd", -- change working directory
+				["cs"] = "actions.change_sort", -- change sorting (name, size, ..)
+				["H"] = "actions.toggle_hidden", -- show/hide hidden files
+			},
+		})
+		KEYMAP("n", "<leader>e", "<CMD>Oil<CR>", keymap_opts("oil: open file explorer"))
+	end,
 }
